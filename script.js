c7f87fc002e4a5e3039856a7f8a1fde9b0b5d586
korotaiev <1059418@gmail.com>
2024-11-02 14:21:05 +0100

    lab5

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.querySelector(".burger").addEventListener("click", function () {
   this.classList.toggle("active");
   document.querySelector(".menu").classList.toggle("open");
 });
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
